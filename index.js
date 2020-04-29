3f981c0fdbfa0d2c9cfa494456ffcfdc20b51c3d
gogovind-arora <govind-arora@github.com>
2020-04-29 14:20:28 +0530

    Express Simple REST

--- index.js
+++ index.js
@@ -1,12 +1,65 @@
 const express = require("express");
 const http = require("http");
 const morgan = require("morgan");
+const bodyParser = require("body-parser");
 
 const hostname = "localhost";
 const port = 3000;
 
 const app = express();
 app.use(morgan("dev"));
+app.use(bodyParser.json());
+
+app.all("/dishes", (req, res, next) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  next();
+});
+
+app.get("/dishes", (req, res, next) => {
+  res.end("Will return dishes to you soon!");
+});
+
+app.post("/dishes", (req, res, next) => {
+  res.end(
+    "Will add the dish: " +
+      req.body.name +
+      "with details" +
+      req.body.description
+  );
+});
+
+app.put("/dishes", (req, res, next) => {
+  res.statusCode = 403;
+  res.end("Put not supported");
+});
+
+app.delete("/dishes", (req, res, next) => {
+  res.end("Deleting all the dishes");
+});
+
+app.get("/dishes/:dishId", (req, res, next) => {
+  res.end("Will return details of dish: " + req.params.dishId);
+});
+
+app.post("/dishes/:dishId", (req, res, next) => {
+  res.statusCode = 403;
+  res.end("Post not supported for dish: " + req.body.name);
+});
+
+app.put("/dishes/:dishId", (req, res, next) => {
+  res.write("Updating the dish: " + req.params.dishId + "\n");
+  res.end(
+    "Will update the dish: " +
+      req.body.name +
+      " with details " +
+      req.body.description
+  );
+});
+
+app.delete("/dishes/:dishId", (req, res, next) => {
+  res.end("Deleting the dish: " + req.params.dishId);
+});
 
 app.use(express.static(__dirname + "/public"));
 
